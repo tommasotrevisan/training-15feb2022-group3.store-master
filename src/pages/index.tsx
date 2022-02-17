de7a1e7697616969720e3d3addaf11aff64dd00b
Tommaso Trevisan <tommaso.trevisan@vtex.com.br>
2022-02-17 13:26:11 +0100

    Tom first tests

---
+++ src/pages/index.tsx
@@ -0,0 +1,84 @@
+import { useSession } from '@faststore/sdk'
+import { graphql } from 'gatsby'
+import { GatsbySeo, JsonLd } from 'gatsby-plugin-next-seo'
+import React from 'react'
+import RenderCMS from 'src/components/RenderCMS'
+import type { PageProps } from 'gatsby'
+import type { HomePageQueryQuery } from '@generated/graphql'
+
+export type Props = PageProps<HomePageQueryQuery>
+
+function Page(props: Props) {
+  const {
+    data: { site, cmsHome },
+    location: { pathname, host },
+  } = props
+
+  const { locale } = useSession()
+
+  const title = site?.siteMetadata?.title ?? ''
+  const siteUrl = `https://${host}${pathname}`
+
+  return (
+    <>
+      {/* SEO */}
+      <GatsbySeo
+        title={title}
+        description={site?.siteMetadata?.description ?? ''}
+        titleTemplate={site?.siteMetadata?.titleTemplate ?? ''}
+        language={locale}
+        canonical={siteUrl}
+        openGraph={{
+          type: 'website',
+          url: siteUrl,
+          title: title ?? '',
+          description: site?.siteMetadata?.description ?? '',
+        }}
+      />
+      <JsonLd
+        json={{
+          '@context': 'https://schema.org',
+          '@type': 'WebSite',
+          url: siteUrl,
+          potentialAction: {
+            '@type': 'SearchAction',
+            target: `${siteUrl}/s/?q={search_term_string}`,
+            'query-input': 'required name=search_term_string',
+          },
+        }}
+      />
+
+      {/*
+        Sections: Components imported from '../components/sections' only.
+        Do not import or render components from any other folder in here.
+        Also, only CMS related components should be rendered in here
+      */}
+      <RenderCMS sections={cmsHome?.sections} />
+      <div>
+        <br/><br/>
+        <p>Take a look at this!</p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/8-KpLN57wOc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+        <br/>
+    </>
+  )
+}
+
+export const query = graphql`
+  query HomePageQuery {
+    site {
+      siteMetadata {
+        title
+        description
+        titleTemplate
+      }
+    }
+    cmsHome {
+      sections {
+        data
+        name
+      }
+    }
+  }
+`
+
+export default Page
